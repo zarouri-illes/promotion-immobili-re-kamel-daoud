72dfd3e15fe18ee51c6867cabdd58bf44db8df47
zarouri-illes <illeszarouri@gmail.com>
2024-02-07 22:52:30 +0100

    idk

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -4,8 +4,8 @@ const Hero = () => {
     return (
         <section className="mt-10 relative  overflow-hidden">
             <div className="space-y-8 px-8 md:px-20">
-                <h1 className="text-secondary text-4xl font-bold md:text-6xl">Trouvez la maison de vos reves</h1>
-                <p className="text-lg font-light text-gray md:text-2xl">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure maxime molestias adipisci neque, quibusdam possimus.</p>
+                <h1 className="text-secondary flex text-4xl font-bold md:text-6xl">Trouvez la maison de vos reves</h1>
+                <p className="text-lg font-light text-gray md:text-2xl">hi Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure maxime molestias adipisci neque, quibusdam possimus.</p>
             </div>
         </section>
     )
