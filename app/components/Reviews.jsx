1e1eb8097d237ae055a1d8575f0ce05f6b3155d0
illes <illeszarouri@gmail.com>
2024-02-27 23:54:45 +0100

    bugs

--- app/components/Reviews.jsx
+++ app/components/Reviews.jsx
@@ -25,36 +25,36 @@ const Reviews = () => {
                         transition={{duration:1}}
                         className="md:text-3xl xl:text-5xl text-2xl font-bold text-secondary">Available Propreties</motion.h2>
 
-            <div class="md:px-[70px] h-auto px-8 w-full py-12 mx-auto">
-                <div className="lg:mb-4 w-full">
-                    <p className="text-primary xl:text-2xl md:text-lg">Lorem ipsum</p>
-                    <h2 className="md:text-3xl xl:text-5xl text-2xl font-bold text-secondary"> lorem ipsum iair dlal xk</h2>
-                </div>
-
-                <div className='mt-8 text-white flex flex-col md:flex-row gap-4'>
-
-                    <motion.div 
-                    initial={{y:-50, opacity:0}}
-                    transition={{ duration: 2}}
-                    animate={isInView ? {y:0, opacity:1}: {}}
-                    onViewportEnter={() => {
-                        setIsInView(true);
-                    }}
-                    className='bg-secondary rounded-[10px] p-8 gap-8 items-start '>
-                        <FaQuoteLeft className="text-4xl" />
-                        <div className="mt-4">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur perferendis libero cumque facere, consequatur dolorem?</p>
-                            <div className='mt-8'>
-                                <h2 className="text-xl font-semibold">Zarouri Illes</h2>
-                                <p>Lorem ipsum dolor sit.</p>
-                                <p className="flex mt-8 text-3xl space-x-4 text-primary"><FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStarHalfAlt /></p>
-                            </div>
+                    <div class="md:px-[70px] h-auto px-8 w-full py-12 mx-auto">
+                        <div className="lg:mb-4 w-full">
+                            <p className="text-primary xl:text-2xl md:text-lg">Lorem ipsum</p>
+                            <h2 className="md:text-3xl xl:text-5xl text-2xl font-bold text-secondary"> lorem ipsum iair dlal xk</h2>
                         </div>
-                    </motion.div>
-                   <div/>
-                </div>
+
+                        <div className='mt-8 text-white flex flex-col md:flex-row gap-4'>
+
+                            <motion.div 
+                                initial={{y:-50, opacity:0}}
+                                transition={{ duration: 2}}
+                                animate={isInView ? {y:0, opacity:1}: {}}
+                                onViewportEnter={() => {
+                                    setIsInView(true);
+                                }}
+                                className='bg-secondary rounded-[10px] p-8 gap-8 items-start '>
+                            <FaQuoteLeft className="text-4xl" />
+                            <div className="mt-4">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur perferendis libero cumque facere, consequatur dolorem?</p>
+                                <div className='mt-8'>
+                                    <h2 className="text-xl font-semibold">Zarouri Illes</h2>
+                                    <p>Lorem ipsum dolor sit.</p>
+                                    <p className="flex mt-8 text-3xl space-x-4 text-primary"><FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStarHalfAlt /></p>
+                                </div>
+                            </div>
+                        </motion.div>
+                    </div>
                 </div>
             </div>
+        </div>
         </section>
     )
 }
