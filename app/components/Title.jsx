86ddbcd53c7c0c49c57ed75f9494bc3f00373a94
illes <illeszarouri@gmail.com>
2024-03-23 10:06:37 -0700

    test

--- app/components/Title.jsx
+++ app/components/Title.jsx
@@ -16,7 +16,15 @@ const Title = (props) => {
                 animate={ inView ? {x: 0, opacity: 1} : {}}
                 transition={{duration:1, delay: 0.3}}
                 className="text-primary xl:text-2xl md:text-lg">{props.miniTitle}
-            </motion.p> 
+            </motion.p>
+
+            <motion.h1
+                initial={{ x: -200, opacity: 0}}
+                onViewportEnter={ () => setInView(true) }
+                animate={ inView ? {x: 0, opacity: 1} : {}}
+                transition={{duration:1, delay: 0.1}}
+                className="text-primary xl:text-2xl md:text-lg">{props.title}
+            </motion.h1> 
         </div>
     )
 }
